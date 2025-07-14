e0000997236efb09987999886c2b59c9655e8b0b
Edgar Morales Sarmiento <71855923+EdgarMorales7@users.noreply.github.com>
2025-07-14 13:49:55 -0600

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,12 @@
 import requests
+import os
 
-ACCESS_TOKEN = "EAAK8vERGItEBPL9yzbLRZCTelRZAEXnb1u9INFL2Gs8f7OUWN6OQ0JybBLz8VcZBRr60ELj3be8nevl3abNJQSNWjqZAp0ZBYUQIZBmHoRbm6kqDZBZAiLJY3UWwd94rwphvDglYZA1bcD1eFlQw01hmgBJNO6wZCjAih8kukZAaaBELBbimQdo3mNZB6LPNlxPZAe76VG9hY4f0cODFhZC1NTbYTWPN0Gt4S4vDpCN9ql5sKZATwZDZD"
-PHONE_NUMBER_ID = "670600796143745"
-DESTINATARIO = "5215626885313"  # tu número de WhatsApp en formato internacional
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID")
+DESTINATARIO = "5215626885313"  # Tu número de WhatsApp
 MENSAJE = "Hola, soy un bot de prueba desde la API de Meta"
 
-url = f"https://graph.facebook.com/v22.0/{PHONE_NUMBER_ID}/messages"
+url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
 headers = {
     "Authorization": f"Bearer {ACCESS_TOKEN}",
     "Content-Type": "application/json"
@@ -20,5 +21,5 @@ body = {
 }
 
 r = requests.post(url, headers=headers, json=body)
-print(r.status_code)
-print(r.text)
+print("📤 Código de respuesta:", r.status_code)
+print("📝 Respuesta:", r.text)
